37125a96caebf9d0dc04e7f03ba05a93351bc14f
vaibhav puri <vaibhav@taggito.com>
2012-12-06 16:46:27 -0600

    second update

--- app/controllers/meals_controller.rb
+++ app/controllers/meals_controller.rb
@@ -23,7 +23,11 @@ class MealsController < ApplicationController
 
 
 	def create
-		@meal = Meal.new(params[:meal])
+		date_value = Date.parse(format_date(params[:meal][:mealdate]))
+		@meal = Meal.new
+		@meal.foodname= params[:meal][:foodname]
+		@meal.category_id= params[:meal][:category_id]
+		@meal.mealdate= date_value
 		@meal.save
 
 		respond_to do |format|
@@ -33,6 +37,20 @@ class MealsController < ApplicationController
     	end	
 	end
 
+	def destroy
+		@meal = Meal.find_by_id(params[:id])
+		@meal.destroy
+		respond_to do |format|
+     	 format.html { redirect_to new_meal_url }
+       end
+	end
 
+	def format_date(orginal_date)
+		@formatted_date=""
+		@formatted_date << orginal_date[6,4] + "/"
+		@formatted_date << orginal_date[0,2] + "/"
+		@formatted_date << orginal_date[3,2] 
+		@formatted_date
+	end
 
 end
