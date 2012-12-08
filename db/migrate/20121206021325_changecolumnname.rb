3c18fcc460dae8220066e808947f25cc395a8609
vaibhav puri <vaibhav@taggito.com>
2012-12-07 22:30:26 -0600

    removing migrations

--- db/migrate/20121206021325_changecolumnname.rb
+++ db/migrate/20121206021325_changecolumnname.rb
@@ -1,7 +1,7 @@
 class Changecolumnname < ActiveRecord::Migration
   def change
-  	rename_column :meals, :foodtype, :category_id
- 	change_column :meals, :category_id, :integer
+ # 	rename_column :meals, :foodtype, :category_id
+ #	change_column :meals, :category_id, :integer
   end
 
 end
